072f9fcc1f30849497a37f7cfe07de1d6581a120
ZL <ray040406@gmail.com>
2024-05-21 18:09:41 +1200

    function

---
+++ function.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void foo();
+int global = 0;
+
+int main() 
+{
+    int local = 1;
+    printf("main: global %d local %d\n", global, local);
+    global = 1;
+    foo();
+    printf("main: global %d local %d\n", global, local);
+}
+
+void foo() {
+    int local; // local to foo
+    local = 3;
+    printf("foo: global %d\n, global");
+    global = 2;
+}
